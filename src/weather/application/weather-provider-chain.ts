ac968b867dd80d4714e60193cb3a0facbe4bc42d
Bogdan Zinovij <bogdanolexandrov@gmail.com>
2025-06-21 17:41:49 +0300

    feat(weather): replace providers chain with stateless implementation

--- src/weather/application/weather-provider-chain.ts
+++ src/weather/application/weather-provider-chain.ts
@@ -2,23 +2,17 @@ import { WeatherProvider } from '../domain/interfaces/weather-provider.interface
 import { WeatherData } from '../domain/types/weather-data.type';
 
 export class WeatherProviderChain {
-  private index = 0;
-
   constructor(private readonly providers: WeatherProvider[]) {}
 
   async getWeather(city: string): Promise<WeatherData> {
-    if (this.index >= this.providers.length) {
-      throw new Error('All weather providers failed');
+    for (const provider of this.providers) {
+      try {
+        return await provider.getWeather(city);
+      } catch {
+        continue;
+      }
     }
 
-    const provider = this.providers[this.index];
-    this.index++;
-
-    try {
-      return await provider.getWeather(city);
-    } catch (error) {
-      console.warn(`[Provider error]: ${error}`);
-      return this.getWeather(city);
-    }
+    throw new Error('All weather providers failed');
   }
 }
